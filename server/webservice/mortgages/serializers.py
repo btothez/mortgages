5af2043b927bd708393a0439b317fe0f4260fc5f
Reisman <boaz.reisman@myhost.local>
2019-09-20 13:07:02 -0400

    API endpoint

---
+++ server/webservice/mortgages/serializers.py
@@ -0,0 +1,4 @@
+from rest_framework import serializers
+
+class PredictionSerializer(serializers.Serializer):
+    prediction = serializers.IntegerField()
--- server/webservice/mortgages/views.py
+++ server/webservice/mortgages/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import viewsets, views
+from rest_framework.response import Response
+from mortgages.serializers import PredictionSerializer
+from rest_framework.response import Response
 
 def index(request):
-    #return HttpResponse("Hello, world. You're at the polls index.")
     context = {}
     return render(request, 'mortgages/index.html', context)
+"""
+class PredictionView(views.APIView):
+    def __init__(self):
+        self.get_extra_actions = []
+        super().__init__() 
 
+    def get(self, request):
+        print('!!!!!!!!!!!!!!')
+        data = {'prediciton': 10}
+        results = PredictionSerializer(data).data
+        return Response(results)
+"""
+
+class PredictionViewSet(viewsets.ViewSet):
+    # Required for the Browsable API renderer to have a nice form.
+    serializer_class = PredictionSerializer
+
+    def list(self, request):
+        serializer = PredictionSerializer({
+            'prediction': 40
+        })
+        return Response(serializer.data)
