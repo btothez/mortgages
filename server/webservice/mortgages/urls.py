e09721f931ced41fa37b2d7a3b01b403e788321f
Reisman <boaz.reisman@myhost.local>
2019-09-20 13:07:02 -0400

    API endpoint

--- server/webservice/mortgages/urls.py
+++ server/webservice/mortgages/urls.py
@@ -1,8 +1,15 @@
 from . import views
 from django.urls import path
-from django.conf.urls import url
+from django.conf.urls import url, include
+from mortgages.views import PredictionViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+
+router.register(r'predictions', PredictionViewSet, base_name='predictions')
 
 urlpatterns = [
-    #path('', views.index, name='index'),
     url(r'^$', views.index, name='index'),
 ]
+urlpatterns += router.urls
+
--- server/webservice/webservice/.#settings.py
+++
@@ -1,1 +0,0 @@
-boaz.reisman@myhost.local.44927
