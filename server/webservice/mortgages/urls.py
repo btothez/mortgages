e64c4ff7ed11d3cf00596ced61935c68ed2db37e
Reisman <boaz.reisman@myhost.local>
2019-09-20 10:25:56 -0400

    Basic Django App

---
+++ server/webservice/mortgages/urls.py
@@ -0,0 +1,8 @@
+from . import views
+from django.urls import path
+from django.conf.urls import url
+
+urlpatterns = [
+    #path('', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
+]
