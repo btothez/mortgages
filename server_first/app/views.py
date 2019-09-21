d961138cd2698036ec538e9d0cc6e911cab1ffa6
Reisman <boaz.reisman@myhost.local>
2019-09-21 08:45:19 -0400

    Cleaning up

--- server_first/app/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    """View function for home page of site."""
-    context = {}
-
-    # Render the HTML template index.html with the data in the context variable
-    return render(request, 'app/index.html', context=context)
--- server_first/mortgages/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MortgagesConfig(AppConfig):
-    name = 'mortgages'
